78698ec850244ec34c201d88cca2869f33d74273
Roshan Thakur <collegemaster098@gmail.com>
2026-08-01 07:49:47 +0530

    Time: 0 ms (100%), Space: 46.8 MB (51.37%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            k--;
+            if (k == 0) {
+                return current.val;
+            }
+            current = current.right;
+        }
+
+        return -1; 
+    }
+}
